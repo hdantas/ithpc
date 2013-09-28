e3331ebc9845e6d7d50fceead7b7eba656b42b01
hdantas <henry.d90@gmail.com>
2013-09-28 14:10:25 +0200

    Finished Challenge OpenMP

--- challenges/2013-09-30 OpenMP/OpenMP.c
+++ challenges/2013-09-30 OpenMP/OpenMP.c
@@ -1,149 +1,153 @@
 #include <stdio.h>
+#include <string.h>
 #include <omp.h>
 #include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
 #include <time.h>
 
-#define DIM 256
-#define MAXNUM 100
+// Number of OpenMP algorithms
+#define NALGORITHMS 3
 
-void reset();
-void init();
-void outerloop();
-void innerloop();
-void seq();
-void print(char *file);
-void twoD();
+// Maximum integer value for matrix and vector
+#define MAXNUMBER 100000
 
-int y[DIM];
-int x[DIM];
-int A[DIM][DIM];
+// Number of iterations
+#define TIMES 500
+
+// Input Size
+#define NSIZE 1
+#define NMAX 256
+// #define NMAX 1447
 
 
-int main() {
+// Seed Input
+int y[NMAX];
+int x[NMAX];
+int A[NMAX][NMAX];
 
-    init();
 
-    printf("Sequential\n");
-    reset();
-    seq();
-    print("seq.txt");
-    
-    printf("Outerloop\n");
-    reset();
-    outerloop();
-    print("outer.txt");
-
-    printf("Innerloop\n");
-    reset();
-    innerloop();
-    print("inner.txt");
-
-    printf("2D\n");
-    reset();
-    twoD();
-    print("2D.txt");
-
-    return 0;
-}
-
-void reset(){
+void init(int dim){
     int i;
-    for( i = 0; i < DIM; i++){
+    for (i = 0; i < dim; i++)
         y[i] = 0;
-    }
-}
-void init() {
-    int i, j;
-    
-    srand(time(NULL) );
-    
-    for(i = 0; i < DIM; i++){
-            x[i] = rand() % MAXNUM;
-    }
-
-    for(i = 0; i < DIM; i++){
-        for(j = 0; j < DIM; j++){
-            A[i][j] = rand() % MAXNUM;
-        }
-    }   
-}
-void outerloop() {
-    int i, j;
-    #pragma omp parallel
-    {
-        #pragma omp parallel for private(j)
-        for(i = 0; i < DIM; i++) {
-            y[i] = 0; 
-            for(j = 0; j < DIM; j++) {
-                y[i] = y[i] + A[i][j] * x[j]; 
-            }
-        }
-    }
 }
 
-void innerloop() {
+void seq_function(int dim){
+        /* The code for sequential algorithm */
     int i, j;
-    int tmp;
-    #pragma omp parallel private(j)
-    {
-        for(i = 0; i < DIM; i++) {
-            tmp = 0;
-            #pragma omp parallel for reduction(+ : tmp)
-            for(j = 0; j < DIM; j++) {
-                tmp = tmp + A[i][j] * x[j];
-            }
-            y[i] = tmp;
-        }
-    }
-}
 
-void seq() {
-    int i, j;
-    for(i = 0; i < DIM; i++) {
+    for(i = 0; i < dim; i++) {
         y[i] = 0; 
-        for(j = 0; j < DIM; j++) {
+        for(j = 0; j < dim; j++) {
             y[i] = y[i] + A[i][j] * x[j];
         }
     }
 }
 
-void print(char *txt) {
-    FILE *f = fopen(txt, "w");
-        
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-        exit(1);
+void omp_outer(int dim){
+    int i, j;
+    
+    #pragma omp parallel for private(j)
+    for(i = 0; i < dim; i++) {
+        y[i] = 0; 
+        for(j = 0; j < dim; j++) {
+            y[i] = y[i] + A[i][j] * x[j]; 
+        }
     }
-
-    int i;
-    for(i = 0; i < DIM; i++) {
-        fprintf(f, "\ty[%d] = %d\n", i, y[i]);
-    }    
-
-    fclose(f);
 }
 
-void twoD() { 
-    int i, j, k, out;
-    int tmp[DIM * DIM];
+void omp_inner(int dim){
+    int i, j, tmp;
 
-    #pragma omp parallel
-    {
-        #pragma omp parallel for private(i, j, k)
-        for (k = 0; k < DIM * DIM; k++) {
-            i = (int) k / DIM;
-            j = k % DIM;
-            tmp[k] = A[i][j] * x[j];
+    for(i = 0; i < dim; i++) {
+        tmp = 0;
+        #pragma omp parallel for reduction(+ : tmp)
+        for(j = 0; j < dim; j++) {
+            tmp = tmp + A[i][j] * x[j];
         }
-
-        for(i = 0; i < DIM; i++) {
-            out = 0;
-            #pragma omp parallel for private(j) reduction(+ : out)
-            for(j = 0; j < DIM; j++) {
-                out += tmp[i * DIM + j];
-            }
-            y[i] = out;
-        }
+        y[i] = tmp;
     }
+}
+
+void omp_both(int dim){
+    int i, j, k, out;
+    int tmp[dim * dim];
+    
+    #pragma omp parallel for private(i, j)
+    for (k = 0; k < dim * dim; k++) {
+        i = (int) k / dim;
+        j = k % dim;
+        tmp[k] = A[i][j] * x[j];
+    }
+
+    for(i = 0; i < dim; i++) {
+        out = 0;
+        #pragma omp parallel for reduction(+ : out)
+        for(j = 0; j < dim; j++) {
+            out = out + tmp[i * dim + j];
+        }
+        y[i] = out;
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    struct timeval startt, endt, result;
+    int i, j, t, n, id;
+    
+    result.tv_sec = 0;
+    result.tv_usec= 0;
+
+    /* Generate a seed input */
+    srand ( time(NULL) );
+    for(i = 0; i < NMAX; i++) {
+            x[i] = rand() % MAXNUMBER;
+    }
+
+    for(i = 0; i < NMAX; i++) {
+        for(j = 0; j < NMAX; j++) {
+            A[i][j] = rand() % MAXNUMBER;
+        }
+    }        
+
+    printf("| NSize | Iterations |    Seq   |    Outer   |    Inner   |    Both    |\n");
+
+    // for each input size
+    n = NMAX;
+    printf("| %5d | %10d |",n,TIMES);
+
+    /* Run sequential algorithm */
+    result.tv_usec=0;
+    gettimeofday (&startt, NULL);
+    for (t=0; t<TIMES; t++) {
+        init(n);
+        seq_function(n);
+    }
+
+    gettimeofday (&endt, NULL);
+    result.tv_usec = (endt.tv_sec*1000000+endt.tv_usec) - (startt.tv_sec*1000000+startt.tv_usec);
+    printf(" %ld.%06ld | ", result.tv_usec/1000000, result.tv_usec%1000000);
+
+    for(id = 0; id < NALGORITHMS; id++) {
+        result.tv_sec=0; result.tv_usec=0;
+        gettimeofday (&startt, NULL);
+        
+        for (t = 0; t < TIMES; t++)
+        {
+            init(n);
+            if (id == 0)
+                omp_outer(n);
+            else if (id == 1)
+                omp_inner(n);
+            else if (id == 2)
+                omp_both(n);
+        }
+        gettimeofday (&endt, NULL);
+
+        result.tv_usec += (endt.tv_sec*1000000+endt.tv_usec) - (startt.tv_sec*1000000+startt.tv_usec);
+        printf(" %ld.%07ld | ", result.tv_usec/1000000, result.tv_usec%1000000);
+    }
+    printf("\n");
+    return 0;
 }
