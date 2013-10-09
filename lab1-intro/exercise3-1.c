dd02de469bbfd6b56c21206abd0551ae38ce2a49
hdantas <henry.d90@gmail.com>
2013-10-09 19:12:49 +0200

    Working on assignment 1. No new function has been tested

--- lab1-intro/exercise3-1.c
+++ lab1-intro/exercise3-1.c
@@ -5,11 +5,20 @@ int np, rank;
 
 int main(int argc, char **argv)
 {
+  int dest;
+  float f = 1.0;
+  MPI_Status status;
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   
-  printf("Node %d of %d says: Hello world!\n", rank, np);
+  if (rank == 0) {
+    f = 4.2;
+  }
+  
+  MPI_Bcast(&f, 1, MPI_FLOAT, rank, MPI_COMM_WORLD);
+  printf("Node %i received %f\n", rank, f);
   
   MPI_Finalize();
   return 0;
